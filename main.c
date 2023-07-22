ba39efb6808770eb28447c4f941f6673ec0a8d25
Puneet-RK <105099669+Puneet-RK@users.noreply.github.com>
2023-07-22 10:52:46 +0530

    BST Inorder preorder and postorder traversal

--- main.c
+++ main.c
@@ -1,138 +1,189 @@
-#include<stdio.h>
-#include<stdlib.h>
-FILE *input,*queue,*enqueue,*dq,*logg;
-struct Node
-{
-    int data;
-    struct Node *next;
+// Binary Search Tree operations in C
+#include <stdio.h>
+#include <stdlib.h>
+FILE *inp,*indr,*predr,*posdr;
+struct Node {
+  int key;
+  struct Node *left, *right;
 };
 typedef struct Node *node;
-node head=NULL,tail=NULL;
 
-void enqueuee()
+// Create a node
+node newNode(int item)
 {
-    node newNode=malloc(sizeof(struct Node));
-    if(newNode==NULL)
+  node temp = (node)malloc(sizeof(struct Node));
+  if(temp==NULL)
+  {
+      printf("\nnode not created!\n");
+      return temp;
+  }
+  temp->key = item;
+  temp->left = temp->right = NULL;
+  return temp;
+}
+
+// Inorder Traversal
+void inorder(node root)
+{
+    if (root!=NULL)
     {
-        printf("\nNode not created!!\n");
-        return;
+        // Traverse left
+        inorder(root->left);
+
+        // Traverse root
+        fprintf(indr,"%d\n",root->key);
+        printf("%d -> ", root->key);
+
+        // Traverse right
+        inorder(root->right);
     }
-    int val;
-    fscanf(input,"%d",&val);
-
-    newNode->data=val;
-    newNode->next = NULL;
-
-    //if it is the first node
-    if(head == NULL && tail == NULL)
-        //make both head and tail points to the new node
-        head = tail = newNode;
-    else
+}
+//Preorder Traversal
+void preorder(node root)
+{
+    if (root!=NULL)
     {
-        //add newnode in tail->next
-        tail->next = newNode;
+        // Traverse root
+        fprintf(predr,"%d\n",root->key);
+        printf("%d -> ", root->key);
 
-        //make the new node as the tail node
-        tail = newNode;
+        // Traverse left
+        preorder(root->left);
+
+        // Traverse right
+        preorder(root->right);
     }
-    queue=fopen("queue.txt","a");
-    fprintf(queue,"%d\n",val);
-    fclose(queue);
-    fprintf(enqueue,"%d\n",val);
-    logg=fopen("log.txt","a");
-    fprintf(logg,"Enqueue\n");
-    fclose(logg);
 }
 
-void dequeuee()
+//Postorder Traversal
+void postorder(node root)
 {
-    //used to free the first node after dequeue
-    node temp;
-
-    if(head == NULL){
-         printf("Queue is Empty. Unable to perform dequeue\n");return;}
-        //take backup
-        temp = head;
-
-        //make the head node points to the next node
-        //logically removing the head element
-        head = head->next;
-
-        //if head == NULL, set tail = NULL
-        if(head == NULL)
-            {tail = NULL;
-            head=NULL;}
-       //free the first node
-       fprintf(dq,"%d\n",temp->data);
-       free(temp);
-       queue=fopen("queue.txt","w");
-       temp=head;
-       while(temp!=NULL)
-       {
-           fprintf(queue,"%d\n",temp->data);
-           temp=temp->next;
-       }
-       fclose(queue);
-        logg=fopen("log.txt","a");
-        fprintf(logg,"Dequeue\n");
-        fclose(logg);
-
-
-}
-
-void display()
-{
-    node temp = head;
-
-    while(temp)
+    if (root!=NULL)
     {
-        printf("%d->",temp->data);
-        temp = temp->next;
+        // Traverse left
+        postorder(root->left);
+
+        // Traverse right
+        postorder(root->right);
+
+        // Traverse root
+        fprintf(posdr,"%d\n",root->key);
+        printf("%d -> ", root->key);
     }
-    printf("NULL\n");
 }
-void random(int n)
+
+
+// Insert a node
+node insert(node nodee, int num)
 {
-    input=fopen("input.txt","w");
-    for(int i=0;i<n;i++)
+  // Return a new node if the tree is empty
+  if (nodee == NULL)
+    return newNode(num);
+
+  // Traverse to the right place and insert the node
+  if (num < nodee->key)
+    nodee->left = insert(nodee->left, num);
+  else
+    nodee->right = insert(nodee->right, num);
+
+  return nodee;
+}
+
+// Find the inorder successor
+node minValueNode(node nodee)
+{
+    node current;
+    current = nodee;
+
+  // Find the leftmost leaf
+    while (current && current->left != NULL)
+    current = current->left;
+
+  return current;
+}
+
+// Deleting a node
+node deleteNode(node root, int num) {
+  // Return if the tree is empty
+  if (root == NULL) return root;
+
+  // Find the node to be deleted
+  if (num < root->key)
+    root->left = deleteNode(root->left, num);
+  else if (num > root->key)
+    root->right = deleteNode(root->right, num);
+
+  else
+{
+    // If the node is with only one child or no child
+    if (root->left == NULL)
     {
-        fprintf(input,"%d\n",rand()%1000);
+      node temp = root->right;
+      free(root);
+      return temp;
+    } else if (root->right == NULL)
+    {
+      node temp = root->left;
+      free(root);
+      return temp;
     }
-    fclose(input);
+
+    // If the node has two children
+    node temp = minValueNode(root->right);
+
+    // Place the inorder successor in position of the node to be deleted
+    root->key = temp->key;
+
+    // Delete the inorder successor
+    root->right = deleteNode(root->right, temp->key);
+  }
+  return root;
 }
- void flush()
+
+void flushfile()
 {
-    queue=fopen("queue.txt","w");
-    fclose(queue);
-    logg=fopen("log.txt","w");
-    fclose(logg);
+    indr=fopen("Inorder.txt","w");
+    predr=fopen("Preorder.txt","w");
+    posdr=fopen("Postorder.txt","w");
+
+    fclose(indr);
+    fclose(predr);
+    fclose(posdr);
 }
-int main()
+void random()
 {
-    int choice,n;
-    printf("Enter the number of random numbers to be generated: ");
-    scanf("%d",&n);
-    random(n);
-    flush();
-    enqueue=fopen("enqueue.txt","w");
-    dq=fopen("dequeue.txt","w");
-    input=fopen("input.txt","r");
+    for(int i=0;i<100;i++)
+    {
+        fprintf(inp,"%d\n",rand()%100);
+    }
+}
+// Driver code
+int main() {
+    node root = NULL;
+    int choice,n,dnk;
+    flushfile();
+    inp=fopen("Input.txt","w");
+    random();
+    fclose(inp);
+    inp=fopen("Input.txt","r");
+
+    printf("Enter choice: \n");
     while(1)
     {
-        printf("\nEnter 1:Enqueue 2:Dequeue 3:Display\n");
+        printf("\n1-Insert, 2-Inorder, 3-Preorder, 4-Postorder, 5-delete, 6-Exit:\n");
         scanf("%d",&choice);
         switch(choice)
         {
-            case 1:enqueuee(); break;
-            case 2:dequeuee(); break;
-            case 3:display(); break;
-            case 4:goto finish;break;
-            default:printf("\nChoice not valid\n");
+            case 1:fscanf(inp,"%d",&n);root=insert(root,n);break;
+            case 2:indr=fopen("Inorder.txt","w");inorder(root);fclose(indr);break;
+            case 3:predr=fopen("Preorder.txt","w");preorder(root);fclose(predr);break;
+            case 4:posdr=fopen("Postorder.txt","w");postorder(root);fclose(posdr);break;
+            case 5:printf("Enter data to be deleted");scanf("%d",&dnk);root=deleteNode(root,dnk);break;
+            case 6:goto finished;break;
+            default: printf("\nInvalid choice!\n");
         }
     }
-    finish:
-    fclose(input);
-    fclose(dq);
-    fclose(enqueue);
-    return 0;
+    finished:
+        printf("\nFinished!\n");
+        fclose(inp);
 }
